a5a5b8e48beb817faaa702875f84b744eb8f80f3
AlexHaxe <Alexander.Blum@gmail.com>
2019-02-21 22:43:14 +0100

    partial filters working

--- test-adapter/testadapter/hexunit/Injector.hx
+++ test-adapter/testadapter/hexunit/Injector.hx
@@ -9,14 +9,50 @@ using testadapter.PatchTools;
 class Injector {
 	public static function buildCore():Array<Field> {
 		var fields = Context.getBuildFields();
+		var isFiltered = (Macro.filters.length != 0);
 		for (field in fields) {
 			switch (field.name) {
 				case "new":
 					field.addInit(macro {
 						addListener(new testadapter.hexunit.Notifier($v{Sys.getCwd()}));
 					});
+					if (isFiltered) {
+						for (filter in Macro.filters) {
+							var parts = filter.split(".");
+							var lastPart = parts.pop();
+							var className = "";
+							if (~/^[A-Z]/.match(lastPart)) {
+								className = filter;
+								trace(className);
+								field.patch(End, macro {
+									var c = Type.resolveClass($v{className});
+									// __addTest(c);
+								});
+							} else {
+								className = parts.join(".");
+								field.patch(End, macro {
+									__addTestMethod(Type.resolveClass($v{className}), $v{lastPart});
+								});
+							}
+						}
+					}
+				case "addTestMethod", "addDescriptor":
+					if (isFiltered) {
+						field.name = "__" + field.name;
+					}
 				case _:
 			}
+		}
+		if (isFiltered) {
+			var extraFields = (macro class {
+				public function addTestMethod(testableClass:Class<Dynamic>, methodName:String):Void {}
+				public function addDescriptor(classDescriptor:ClassDescriptor):Void {}
+				// public function __addTest(testableClass:Class<Dynamic>) {
+				// 	var descriptor = ClassDescriptorGenerator.doGeneration(testableClass);
+				// 	return __addDescriptor(descriptor);
+				// }
+			}).fields;
+			return fields.concat(extraFields);
 		}
 		return fields;
 	}
