b53fde652ba72af3e8f56a2bcac1ed8885ea2063
Kevin Leung <kevinresol@gmail.com>
2019-02-22 21:00:27 +0800

    Cater for upstream changes

--- test-adapter/testadapter/tinktestrunner/Injector.hx
+++ test-adapter/testadapter/tinktestrunner/Injector.hx
@@ -5,6 +5,24 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 
 class Injector {
+	public static function buildCase():Array<Field> {
+		var fields = Context.getBuildFields();
+		for (field in fields) {
+			switch (field.name) {
+				case "shouldRun":
+					field.name = "__" + field.name;
+				case _:
+			}
+		}
+
+		var extraFields = (macro class {
+			public function shouldRun(includeMode:Bool):Bool {
+				return testadapter.data.TestFilter.shouldRunTest($v{Macro.filters}, clazz, caze.info.name) && __shouldRun(includeMode);
+			}
+		}).fields;
+		return fields.concat(extraFields);
+	}
+
 	public static function buildRunner():Array<Field> {
 		var fields = Context.getBuildFields();
 		for (field in fields) {
@@ -24,20 +42,12 @@ class Injector {
 				adapterReporter = new testadapter.tinktestrunner.Reporter($v{Sys.getCwd()}, reporter);
 				return __run(batch, adapterReporter, timers);
 			}
-			static function runCase(caze:Case, suite:Suite, reporter:Reporter, timers:TimerManager):Future<CaseResult> {
+			static function runCase(caze:Case, suite:Suite, reporter:Reporter, timers:TimerManager, shouldRun:Bool):Future<CaseResult> {
 				var clazz:Null<String> = adapterReporter.testResults.positions.resolveClassName(caze.pos.fileName, caze.pos.lineNumber - 1);
 				if (clazz == null) {
 					clazz = suite.info.name;
 				}
-				if (!testadapter.data.TestFilter.shouldRunTest($v{Macro.filters}, clazz, caze.info.name)) {
-					return Future.async(function(cb) {
-						cb({
-							info: caze.info,
-							results: Outcome.Success([])
-						});
-					});
-				}
-				return __runCase(caze, suite, reporter, timers);
+				return __runCase(caze, suite, reporter, timers, shouldRun);
 			}
 		}).fields;
 		return fields.concat(extraFields);
