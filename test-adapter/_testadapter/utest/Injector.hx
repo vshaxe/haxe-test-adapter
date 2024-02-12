f6bc0bf58809c928c2402238b35ab1b37cb1fb51
AlexHaxe <Alexander.Blum@gmail.com>
2024-02-12 01:11:01 +0100

    fixed utest 2.x filter support

--- test-adapter/_testadapter/utest/Injector.hx
+++ test-adapter/_testadapter/utest/Injector.hx
@@ -13,7 +13,38 @@ class Injector {
 			switch (field.name) {
 				case "new":
 					field.addInit(macro new _testadapter.utest.Reporter(this, $v{Sys.getCwd()}));
-				case "addITest":
+				#if (utest >= version("2.0.0-alpha"))
+				case "addCase": // utest 2.x support
+					field.patch(Replace, macro {
+						var className = Type.getClassName(Type.getClass(testCase));
+						if (fixtures.exists(className)) {
+							throw new UTestException('Cannot add the same test twice.');
+						}
+						var newFixtures = [];
+						var init:utest.TestData.InitializeUtest = (cast testCase : utest.TestData.Initializer).__initializeUtest__();
+						var cls:_testadapter.data.Data.SuiteId = ClassName(className);
+						for (test in init.tests) {
+							if (!isTestFixtureName(className, test.name, ['test', 'spec'], pattern, globalPattern)) {
+								continue;
+							}
+							if (!_testadapter.data.TestFilter.shouldRunTest($v{Macro.filters}, cls, test.name)) {
+								continue;
+							}
+							newFixtures.push(new utest.TestFixture(testCase, test, init.accessories));
+						}
+						if (newFixtures.length > 0) {
+							fixtures.set(className, {
+								caseInstance: testCase,
+								setupClass: init.accessories.getSetupClass(),
+								dependencies: #if UTEST_IGNORE_DEPENDS [] #else init.dependencies #end,
+								fixtures: newFixtures,
+								teardownClass: init.accessories.getTeardownClass()
+							});
+							length += newFixtures.length;
+						}
+					});
+				#else
+				case "addITest": // utest 1.x support
 					field.patch(Replace, macro {
 						var className = Type.getClassName(Type.getClass(testCase));
 						if (iTestFixtures.exists(className)) {
@@ -44,6 +75,7 @@ class Injector {
 							teardownClass: init.accessories.getTeardownClass()
 						});
 					});
+				#end
 			}
 		}
 		return fields;
